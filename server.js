710691a0c1fb383fe52ee6f19abf16192fe0909e
AnhMHNguyen <84159499+AnhMHNguyen@users.noreply.github.com>
2021-10-04 01:16:00 -0700

    first deploy

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
